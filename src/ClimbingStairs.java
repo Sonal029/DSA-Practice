96f12420b5c2dcde6700d8857ba8e1126e71a008
Shakambhari <shakambhari.1@hhstechgroup.com>
2024-10-22 15:54:47 +0530

    Climbing Stairs

--- src/ClimbingStairs.java
+++ src/ClimbingStairs.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
 public class ClimbingStairs {
+    public static void main(String[] args) {
+
+        System.out.println(climbStairs(5));
+
+    }
+
+    public static int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+
+        int oneStepBefore = 2;
+        int twoStepsBefore = 1;
+        int allWays = 0;
+
+        for (int i = 3; i <= n; i++) {
+            allWays = oneStepBefore + twoStepsBefore;
+            twoStepsBefore = oneStepBefore;
+            oneStepBefore = allWays;
+        }
+
+        return allWays;
+    }
+
 }
